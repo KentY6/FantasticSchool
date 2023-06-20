107ed741d853e52e6a07e6e9fa0a234b594862ba
KentY <e6ony.ivory@gmail.com>
2023-06-20 16:31:29 +0900

    コンポーネントが更新される度に一番下にスクロールされるよう設定

--- src/components/TalkArea.js
+++ src/components/TalkArea.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { StyleSheet, ScrollView } from "react-native";
 import { StudentsTalkArea } from "./StudentsTalkArea";
 import { TeachersTalkArea } from "./TeachersTalkArea";
 
 export const TalkArea = ({ isActiveTeacher, conversationLog, translation }) => {
+  const scrollRef = useRef();
+
+  // コンポーネントが更新される度に一番下にスクロールされるように設定
+  useEffect(() => {
+    scrollRef.current.scrollToEnd({ animated: true });
+  });
+
   return (
-    <ScrollView style={styles.talkArea}>
+    <ScrollView style={styles.talkArea} ref={scrollRef}>
       {conversationLog.map((data, index) => {
         const isTeachersText = data.whoseText === "teacher";
         return isTeachersText ? (
